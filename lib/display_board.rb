98ff670467501d88524743e7de5acf57e157569e
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-16 23:16:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
-# Define display_board that accepts a board and prints
+def board(numbers)
+  puts "   "|"   "|"   "
+  puts "-----------"
+  puts "   "|"   "|"   "
+  puts "-----------"
+  puts "   "|"   "|"   "
+  end
+  
+  # Define display_board that accepts a board and prints
 # out the current state.
