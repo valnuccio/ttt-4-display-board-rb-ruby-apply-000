5216080cfff99cec7a7f71b56fe1d22faf244866
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-16 23:23:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,15 @@
+a=["x","x","x","x","x","x","x","x","x"]
+
 def board(numbers)
-  puts "   "|"   "|"   "
+  puts " #{numbers[0]} | #{numbers[1]} | #{numbers[2]}"
   puts "-----------"
-  puts "   "|"   "|"   "
+  puts " #{numbers[3]} | #{numbers[4]} | #{numbers[5]} "
   puts "-----------"
-  puts "   "|"   "|"   "
+  puts " #{numbers[6]} | #{numbers[7]} | #{numbers[8]} "
   end
   
   # Define display_board that accepts a board and prints
 # out the current state.
+
+def board(a)
+  
